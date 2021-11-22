489f9b6d431895cbb85eefc154671c122f4af168
mohamed safras <92223444+safras12@users.noreply.github.com>
2021-11-22 22:36:00 +0530

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,3 +1,24 @@
+
+function chargebattery() {
+  var a;
+  a = document.getElementById("charging");
+  a.innerHTML = "&#xf244;";
+  setTimeout(function () {
+    a.innerHTML = "&#xf243;";
+  }, 1000);
+  setTimeout(function () {
+    a.innerHTML = "&#xf242;";
+  }, 2000);
+  setTimeout(function () {
+    a.innerHTML = "&#xf241;";
+  }, 3000);
+  setTimeout(function () {
+    a.innerHTML = "&#xf240;";
+  }, 4000);
+}
+chargebattery();
+setInterval(chargebattery, 5000);
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
